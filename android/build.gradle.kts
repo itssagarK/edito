a59d4d9054734c27bb4b1909ca497579eb80f466
itssagarK <sagarkapri321@gmail.com>
2026-09-02 20:42:15 +0530

    ci: configure safe non-app evaluationDependsOn and Java 1.8 compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,3 +4,21 @@ allprojects {
         mavenCentral()
     }
 }
+
+val newBuildDir: java.io.File = rootProject.projectDir.resolve("../build")
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: java.io.File = newBuildDir.resolve(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+}
+
+subprojects {
+    if (project.path != ":app") {
+        project.evaluationDependsOn(":app")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
