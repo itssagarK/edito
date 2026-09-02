8d429a927f21c00944e6f4dcf6e4257298f6c19b
itssagarK <sagarkapri321@gmail.com>
2026-09-02 20:47:38 +0530

    ci: clean root build.gradle.kts and add gradle assembleDebug fallback

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,20 +5,6 @@ allprojects {
     }
 }
 
-val newBuildDir: java.io.File = rootProject.projectDir.resolve("../build")
-rootProject.layout.buildDirectory.set(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: java.io.File = newBuildDir.resolve(project.name)
-    project.layout.buildDirectory.set(newSubprojectBuildDir)
-}
-
-subprojects {
-    if (project.path != ":app") {
-        project.evaluationDependsOn(":app")
-    }
-}
-
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
